5d4dc19fe53d30678c352cec0f52527549b5ba83
prabhanjansp <puranikchetan97@gmail.com>
2025-03-10 14:50:00 +0530

    new spotLight app with typescript

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import {styles} from "../styles/auth.styles"
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+
+      <Text  style={styles.title}>Jai shree ram🚩</Text>
+    </View>
+  );
+}
